c59c1700e869c97372ed986875538aefc799fad0
AlexC <pt!PMzX}ZF>
2025-09-10 14:17:17 +0200

    Added Markdown anchor

--- src/components/MarkdownViewer.vue
+++ src/components/MarkdownViewer.vue
@@ -1,6 +1,28 @@
+<!-- 
+  MarkdownViewer Component Documentation
+  ---------------------------------------
+  This component renders Markdown content as HTML using Vue's computed properties.
+
+  Libraries required:
+    - MarkdownIt: Primary library for parsing Markdown. (npm install markdown-it)
+    - markdown-it-highlightjs: Plugin for syntax highlighting in code blocks. (npm install -D highlight.js)
+    - markdown-it-anchor: Plugin that automatically produces anchor links for headings. (npm install markdown-it-anchor)
+    - DOMPurify: Library to sanitize the rendered HTML and prevent potential XSS vulnerabilities. (npm install dompurify)
+    - Taiwind Prose: Auto Tailwind design npm install -D @tailwindcss/typography
+
+  Component Props:
+    - source (String, required): The Markdown text to be converted to HTML.
+    - class (String, optional): Additional CSS classes to be applied to the container element.
+    - sanitize (Boolean, optional): Flag to indicate whether the rendered HTML should be sanitized. Defaults to true.
+
+  Notes:
+    - The Markdown is rendered with support for HTML, linkification, and typographic enhancements.
+    - The computed property 'renderedHtml' processes and optionally sanitizes the output before it is rendered.
+-->
 <script setup>
 import MarkdownIt from 'markdown-it'
 import hljsPlugin from 'markdown-it-highlightjs'
+import anchor from 'markdown-it-anchor'
 import DOMPurify from 'dompurify'
 import { computed } from 'vue'
 
@@ -10,7 +32,9 @@ const props = defineProps({
   sanitize: { type: Boolean, default: true },
 })
 
-const md = new MarkdownIt({ html: true, linkify: true, typographer: true }).use(hljsPlugin)
+const md = new MarkdownIt({ html: true, linkify: true, typographer: true })
+  .use(hljsPlugin)
+  .use(anchor)
 const renderedHtml = computed(() => {
   let html = md.render(props.source)
   if (props.sanitize) html = DOMPurify.sanitize(html)
