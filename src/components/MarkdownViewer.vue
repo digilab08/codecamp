2a0bd8651c4b7c9c979340b6a18362d48048bd70
Alexander Coopmans <alexander.coopmans@protonmail.com>
2025-10-05 15:11:01 +0200

    feat: Added copy code buttons to markdown code blocks

--- src/components/MarkdownViewer.vue
+++ src/components/MarkdownViewer.vue
@@ -24,7 +24,7 @@ import MarkdownIt from 'markdown-it'
 import hljsPlugin from 'markdown-it-highlightjs'
 import anchor from 'markdown-it-anchor'
 import DOMPurify from 'dompurify'
-import { computed } from 'vue'
+import { computed, onUpdated, onMounted, onUnmounted, nextTick } from 'vue'
 
 const props = defineProps({
   source: { type: String, required: true },
@@ -32,13 +32,70 @@ const props = defineProps({
   sanitize: { type: Boolean, default: true },
 })
 
+const copyCodePlugin = (md) => {
+  const defaultRender =
+    md.renderer.rules.fence ||
+    function (tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options)
+    }
+
+  md.renderer.rules.fence = (tokens, idx, options, env, self) => {
+    const token = tokens[idx]
+    const codeBlock = defaultRender(tokens, idx, options, env, self)
+    const code = token.content
+
+    if (token.info) {
+      const encodedCode = encodeURIComponent(code)
+      const copyButton = `
+        <button class="copy-code-btn btn btn-ghost btn-circle absolute top-3 right-3 material-icons" onclick="copyCode(this)" title="Copy Code" data-code="${encodedCode}">
+          content_copy
+        </button>
+      `
+      return `<div class="relative">${copyButton}${codeBlock}</div>`
+    }
+
+    return codeBlock
+  }
+}
+
 const md = new MarkdownIt({ html: true, linkify: true, typographer: true })
   .use(hljsPlugin)
   .use(anchor)
+  .use(copyCodePlugin)
 const renderedHtml = computed(() => {
   let html = md.render(props.source)
   if (props.sanitize) html = DOMPurify.sanitize(html)
   return html
+})
+
+let elements = []
+const onCopyClick = (event) => {
+  const el = event.currentTarget
+  const code = decodeURIComponent(el.getAttribute('data-code'))
+  navigator.clipboard.writeText(code).then(() => {
+    el.innerText = 'check'
+    setTimeout(() => {
+      el.innerText = 'content_copy'
+    }, 1000)
+  })
+}
+
+function removeEventListeners() {
+  elements.forEach((el) => {
+    el.removeEventListener('click', onCopyClick)
+  })
+}
+
+onUpdated(() => {
+  removeEventListeners()
+  elements = document.querySelectorAll('.copy-code-btn')
+  elements.forEach((el) => {
+    el.addEventListener('click', onCopyClick)
+  })
+})
+
+onUnmounted(() => {
+  removeEventListeners()
 })
 </script>
 
